c9422c9bd3bd096492fb91ee42c515f84e3eb284
Ubuntu <abgelehnt@foxmail.com>
2017-06-26 05:01:43 -0400

    php/->website/

---
+++ website/index.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once("dbsql.php");
+?>
+
+
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>chi</title>
+		<link rel="stylessheet" href="./main.css">
+	</head>
+
+
+	<body>
+
+		<div class="main_div">
+			<form >
+				<input type="text" />
+				<input type="password" />
+				<input type="button" value="login" />
+			</form>
+		</div>
+	<p id="main_id">hello</p>
+
+	</body>
+</html>
