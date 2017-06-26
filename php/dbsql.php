5c176ed2e1271ea26cc05c2bd0e04becbc75676f
Ubuntu <abgelehnt@foxmail.com>
2017-06-26 03:27:12 -0400

    dbsql

---
+++ php/dbsql.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//link to mysql system
+$link = new mysqli('localhost', 'root', '66200519a', 'website');
+$link->query("SET NAMES UTF8");
+
+
+?>	
---
+++ php/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include(desql.php);
+?>
+
+
+<!doctype html>
+<html>
+	<head>
+		<meta chaeset="utf-8">
+		<title>chi</title>
+	</head>
+
+
+	<body>
+	<?php
+
+	
+	?>
+	<br>
+	<hr>
+	<p>The end.</p>
+	</body>
+</html>
