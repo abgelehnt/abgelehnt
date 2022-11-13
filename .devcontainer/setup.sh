fc55d5e0e13c098375dbe388ca94ca9d0dfcbb41
Chi <abgelehnt@foxmail.com>
2022-11-13 10:19:17 +0800

    a

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,5 @@
 ## update and install some things we should probably have
 apt update
-apt install -y \
-  curl \
-  git \
-  gnupg2 \
-  jq \
-  sudo \
-  zsh \
-  vim \
-  build-essential \
-  openssl
+apt install -y curl git gnupg2 jq sudo zsh vim build-essential make openssl ruby ruby-dev 
+gem install jekyll
+
