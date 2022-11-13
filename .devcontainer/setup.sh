f8a6589001ccaff664c7449c25d2ffc407025799
Chi <abgelehnt@foxmail.com>
2022-11-13 10:09:20 +0800

    a

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+## update and install some things we should probably have
+apt update
+apt install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl
