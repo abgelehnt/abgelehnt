cc888b26ea8dba167c13f0428cbc530e30ecdd79
Chi Zhang <abgelehnt@foxmail.com>
2025-05-04 19:05:11 +0800

    Delete .devcontainer directory

--- .devcontainer/setup.sh
+++
@@ -1,5 +0,0 @@
-## update and install some things we should probably have
-apt update
-apt install -y curl git gnupg2 jq sudo zsh vim build-essential make openssl ruby ruby-dev 
-gem install jekyll
-
